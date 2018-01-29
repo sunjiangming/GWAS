7b77d8a177be31c471250c3cef47f09550528733
Jiangming Sun <sunjiangming@gmail.com>
2018-01-29 15:47:39 +0100

    Update partitePlot.R

--- partitePlot.R
+++ partitePlot.R
@@ -31,8 +31,9 @@ for( i in 1:dim(ppi)[1]){
 }
 dymean = aggregate(ppi$yp, list(ppi$interP), mean)
 dymax = aggregate(ppi$yp, list(ppi$interP), max)
+--------------------------------------------------
 y1=dymean
-xy1=sapply(1:dim(dymax[1]), function(i) { if ( dymean[i,2] == dymax[i,2]) y1[i,2]=runif(1,dymean[i,2]-spanLenBait/2, dymean[i,2]+spanLenBait/2) } )
+y1=sapply(1:dim(dymax)[1], function(i) { if ( dymean[i,2] == dymax[i,2]) runif(1,dymean[i,2]-spanLenBait/2, dymean[i,2]+spanLenBait/2); else y1[i,2]} )
 
 
 #maxY=freq$counts
